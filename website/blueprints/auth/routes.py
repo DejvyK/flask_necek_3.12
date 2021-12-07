ab563fd27152baa6c7f5cb66aee01cbef1de91fb
Barrera <barreraalexander93@gmail.com>
2021-12-06 19:13:39 -0500

    this is a simple skeleton update. the app has been reorganized

--- website/blueprints/auth/routes.py
+++ website/blueprints/auth/routes.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, logout_user, login_user
+
+from website import db, bcrypt, login_manager
+from website.models.users import User
+
 
 auth = Blueprint('auth', __name__)
 
@@ -8,9 +13,14 @@ def login():
     print(data)
     return render_template("login.html", boolean=True)
 
-@auth.route('/logout')
+@auth.route("/logout")
+@login_required
 def logout():
-    return '<p> logout </p>'
+    try:
+        logout_user()
+    except Exception as err:
+        print (err)
+        return 'error'
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -34,7 +44,7 @@ def sign_up():
     return render_template("sign_up.html")
 
 
-@app.route('/', methods= ['GET'])
+@auth.route('/', methods= ['GET'])
 def stuff():
     stahp = "1"
     return jsonify(stahp)
