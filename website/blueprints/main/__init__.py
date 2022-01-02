5be6291896b4cc204fae1c1fec2848bd9f9ac50b
DejvyK <93859171+DejvyK@users.noreply.github.com>
2022-01-02 16:37:45 +0100

    Revert "Model update"

--- website/blueprints/main/__init__.py
+++
@@ -1,3 +0,0 @@
-def unpack_elems(elems):
-    return " ".join(elems)
-
--- website/models/users.py
+++ website/models/users.py
@@ -1,93 +1,16 @@
 from flask_login import UserMixin
-from website import login_manager
-from website.models.master import Model
+from website import login_manager, db
 
 
 @login_manager.user_loader
-def load_user(_id):
-    return User.get(by="_id", value=_id)
+def load_user(id):
+    auth_model = ""
 
-class User(Model, UserMixin):
-    mtype = 'user'
-    tablename = 'users'
+    return auth_model
 
-    @classmethod
-    def get_insert_statement(cls, model):
-        statement = (f"""
-        INSERT INTO {cls.tablename}
-            (_id, email, fname, lname, password, admin)
-        VALUES
-            (%s, %s, %s, %s, %s, %s)
-        """)
-        
-        insertions = [model._id, model.email,
-            model.fname, model.lname, model.password,
-            model.admin]
-
-        return statement, insertions
-
-
-    @classmethod
-    def get_table_statement(cls):
-        statement = (f"""
-        CREATE TABLE {cls.tablename}(
-            _id varchar(30) PRIMARY KEY,
-            email varchar(30) NOT NULL UNIQUE,
-            fname varchar(30),
-            lname varchar(30),
-            password varchar(100) NOT NULL,
-            admin int DEFAULT 0,
-            upldate datetime DEFAULT CURRENT_TIMESTAMP(),
-            moddate datetime DEFAULT CURRENT_TIMESTAMP()
-        )
-        """)
-        return statement
-
-    @classmethod
-    def get_update_statement(cls, model):
-        statement = (f""" UPDATE {cls.tablename}
-        SET
-            email = %s,
-            fname = %s,
-            lname = %s,
-            password = %s,
-            moddate = CURRENT_TIMESTAMP()
-        WHERE
-            _id = %s
-        """)
-
-        updates = [model.email, model.fname,
-            model.lname, model.password]
-
-        return statement, updates
-
-    def __init__(self, mdict):
-        super().__init__(mdict)
-        self.email = mdict['email']
-        self.fname = mdict['fname']
-        self.lname = mdict['lname']
-        self.password = mdict['password']
-        self.admin = mdict['admin']
-    
-    @property
-    def as_dict(self):
-        return {
-            "_id" : self._id,
-            "email" : self.email,
-            "fname" : self.fname,
-            "lname" : self.lname,
-            "moddate" : self.moddate,
-            "upldate" : self.upldate,
-        }
-    
-
-    def __str__(self):
-        return (f"""
-        _id : {self._id}
-        fname : {self.fname}
-        admin : {self.admin}
-
-        upload date : {self.upldate}
-        last modified : {self.moddate}
-        
-        """)
+class User(UserMixin):
+    pass
+    # id = db.Column(db.Integer, primary_key=True)
+    # email = db.Column(db.String(150), unique=True)
+    # password = db.Column(db.String(150))
+    # krestni = db.Column(db.String(150))
