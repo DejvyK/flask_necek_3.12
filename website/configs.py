7591f7d4ffe118ec2d6a6d8d7784e386444b062a
barreraalexander <barreraalexander93@gmail.com>
2022-02-08 21:33:41 -0500

    updated to work with local db

--- website/configs.py
+++ website/configs.py
@@ -1,12 +1,15 @@
 import json
 from pathlib import Path
+from os import path as os_path
 
-with open(Path("./website/public_config.json")) as config_file:
+dev = os_path.expanduser("~/etc/upwork_config.json")
+prod = Path("./website/public_config.json")
+
+with open(dev) as config_file:
     config = json.load(config_file)
 
 class Config:
     SECRET_KEY = config.get('SECRET_KEY')
-
     MYSQL_USER = config.get('MYSQL_USER')
     MYSQL_PASSWORD = config.get('MYSQL_PASSWORD')
     MYSQL_HOST = config.get('MYSQL_HOST')
